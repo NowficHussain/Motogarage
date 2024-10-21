48daea485b02f6bc1467c0919c8ecb89722ade87
Nowfic hussain <nowfichussain@gmail.com>
2024-10-21 16:08:55 +0530

    Add files via upload

---
+++ jobs/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Job
+
+
+# Create your views here.
+def homepage(request):
+    jobs = Job.objects.all()
+    return render(request, 'home.html', {'jobs': jobs})
+
+def details(request,job_id):
+    print(job_id)
+    job = get_object_or_404(Job,pk=job_id)
+    return render(request,'Details.html',{'job':job})
